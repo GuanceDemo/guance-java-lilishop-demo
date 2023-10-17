0488ea9d434d3201632572cf9e1efb6eec59aef1
Angelus-zhou <57707829+Angelus-zhou@users.noreply.github.com>
2023-10-17 18:19:17 +0800

    Update docker-build.sh

--- src/lilishop-ui/docker-build.sh
+++ src/lilishop-ui/docker-build.sh
@@ -1,18 +1,14 @@
 #代码目录
 code_path=$PWD
-REGISTRY_PATH='registry.cn-hangzhou.aliyuncs.com/lilishop-ui'
+REGISTRY_PATH='pubrepo.jiagouyun.com/demo'
 BUILD_VERSION=4.2.4.1
 
-read -p "请输入仓库地址": REGISTRY_PATH
+docker build -t $REGISTRY_PATH/demo-lilishop-buyer-ui:$BUILD_VERSION ${code_path}/buyer
+docker push $REGISTRY_PATH/demo-lilishop-buyer-ui:$BUILD_VERSION
 
-echo $REGISTRY_PATH
+docker build -t $REGISTRY_PATH/demo-lilishop-seller-ui:$BUILD_VERSION ${code_path}/seller
+docker push $REGISTRY_PATH/demo-lilishop-seller-ui:$BUILD_VERSION
 
-docker build -t $REGISTRY_PATH/buyer-ui:$BUILD_VERSION ${code_path}/buyer
-docker push $REGISTRY_PATH/buyer-ui:$BUILD_VERSION
-
-docker build -t $REGISTRY_PATH/seller-ui:$BUILD_VERSION ${code_path}/seller
-docker push $REGISTRY_PATH/seller-ui:$BUILD_VERSION
-
-docker build -t $REGISTRY_PATH/manager-ui:$BUILD_VERSION ${code_path}/manager
-docker push $REGISTRY_PATH/manager-ui:$BUILD_VERSION
+docker build -t $REGISTRY_PATH/demo-lilishop-manager-ui:$BUILD_VERSION ${code_path}/manager
+docker push $REGISTRY_PATH/demo-lilishop-manager-ui:$BUILD_VERSION
 
